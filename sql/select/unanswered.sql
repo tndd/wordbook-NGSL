ec02d8b1b0aaf8cbeb91a234130722391d711ec5
tan <ttru702@gmail.com>
2020-12-03 19:48:48 +0900

    update to select only unanswered

--- sql/select/unanswered.sql
+++ sql/select/unanswered.sql
@@ -1,7 +1,5 @@
--- get a list of incorrect or unanswered words.
--- args: [class, version, type]
 select w.id, w.word, w.translation, w.explanation, w.example, w.example_translation, w.pronunciation, w.meaning_in_english 
 from word w
 left join (select * from test where version=? and version=?) t
 on (w.id=t.word_id)
-where "type"=? and (t.correct=0 or t.correct is NULL);
+where "type"=? and t.correct is NULL;
