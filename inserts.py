415ad681ef9746aea977708bf15dd352951eecc9
tan <ttru702@gmail.com>
2020-12-03 15:28:15 +0900

    add insert bsl

--- inserts.py
+++ inserts.py
@@ -11,11 +11,20 @@ def read_values(d_type):
       values.append((d_type, row[0], row[1], row[4], row[5], row[6], row[2], row[3]))
   return values
 
+def read_bsl_values():
+  values = []
+  with open(f'./resource/bsl.csv', 'r') as f:
+    render = csv.reader(f)
+    for row in list(render)[1:]:
+      values.append(('bsl', row[0], row[1], row[3], row[4], row[5], row[2], ''))
+  return values
+
 db_path = 'db.sqlite'
 con = sqlite3.connect(db_path)
 cur = con.cursor()
 cur.executemany(SQL_INSERTS, read_values('ngsl'))
 cur.executemany(SQL_INSERTS, read_values('nawl'))
 cur.executemany(SQL_INSERTS, read_values('tsl'))
+cur.executemany(SQL_INSERTS, read_bsl_values())
 con.commit()
 con.close()
