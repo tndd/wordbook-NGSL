7616cc2b8d19c0da654b870a5bd86cb431a3ddd4
tan <ttru702@gmail.com>
2020-12-03 18:57:45 +0900

    add sql select incorrect answers

---
+++ sql/select/incorrect.sql
@@ -0,0 +1,5 @@
+select w.id, w.word, w.translation, w.explanation, w.example, w.example_translation, w.pronunciation, w.meaning_in_english 
+from word w
+left join (select * from test where version=? and version=?) t
+on (w.id=t.word_id)
+where "type"=? and t.correct=0;
