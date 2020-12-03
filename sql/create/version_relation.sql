927a496e51b7b571f2ac96df9e4ed94fb6b3ac1a
tan <ttru702@gmail.com>
2020-12-03 23:41:22 +0900

    update not null

--- sql/create/version_relation.sql
+++ sql/create/version_relation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE version_relation (
 	id TEXT NOT NULL,
-	parent_id TEXT,
+	parent_id TEXT NOT NULL,
 	"timestamp" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	PRIMARY KEY(id, parent_id),
 	foreign key(id) references version(id),
