25a8515e9242f3c2d70a0e42296a5f3afcb9e82f
tan <ttru702@gmail.com>
2020-12-04 13:54:44 +0900

    rename to db_command

--- db_command.py
+++ db_command.py
@@ -1,6 +1,7 @@
 import csv
 import sqlite3
 
+# utils
 def read_values(d_type):
   values = []
   with open(f'./resource/{d_type}.csv', 'r') as f:
@@ -12,33 +13,35 @@ def read_values(d_type):
         values.append((d_type, row[0], row[1], row[4], row[5], row[6], row[2], row[3]))
   return values
 
+def get_queries():
+  q = {}
+  with open('sql/create/word.sql', 'r') as f:
+    q['CREATE_WORD'] = f.read()
+  with open('sql/create/test.sql') as f:
+    q['CREATE_TEST'] = f.read()
+  with open('sql/create/version_relation.sql') as f:
+    q['VERSION_RELATION'] = f.read()
+  with open('sql/create/version.sql', 'r') as f:
+    q['CREATE_VERSION'] = f.read()
+  with open('sql/insert/word.sql', 'r') as f:
+    q['INSERTS_WORD'] = f.read()
+  return q
 
 if __name__ == "__main__":
-  # read sqls
-  with open('sql/create/word.sql', 'r') as f:
-    SQL_CREATE_WORD = f.read()
-  with open('sql/create/test.sql') as f:
-    SQL_CREATE_TEST = f.read()
-  with open('sql/create/version_relation.sql') as f:
-    SQL_CREATE_VERSION_RELATION = f.read()
-  with open('sql/create/version.sql', 'r') as f:
-    SQL_CREATE_VERSION = f.read()
-  with open('sql/insert/word.sql', 'r') as f:
-    SQL_INSERTS_WORD = f.read()
-
   # execute queries
+  q = get_queries()
   db_path = 'db.sqlite'
   connection = sqlite3.connect(db_path, isolation_level='EXCLUSIVE')
   cur = connection.cursor()
   try:
-    cur.execute(SQL_CREATE_WORD)
-    cur.execute(SQL_CREATE_TEST)
-    cur.execute(SQL_CREATE_VERSION)
-    cur.execute(SQL_CREATE_VERSION_RELATION)
-    cur.executemany(SQL_INSERTS_WORD, read_values('ngsl'))
-    cur.executemany(SQL_INSERTS_WORD, read_values('nawl'))
-    cur.executemany(SQL_INSERTS_WORD, read_values('tsl'))
-    cur.executemany(SQL_INSERTS_WORD, read_values('bsl'))
+    cur.execute(q['CREATE_WORD'])
+    cur.execute(q['CREATE_TEST'])
+    cur.execute(q['CREATE_VERSION'])
+    cur.execute(q['CREATE_VERSION_RELATION'])
+    cur.executemany(q['INSERTS_WORD'], read_values('ngsl'))
+    cur.executemany(q['INSERTS_WORD'], read_values('nawl'))
+    cur.executemany(q['INSERTS_WORD'], read_values('tsl'))
+    cur.executemany(q['INSERTS_WORD'], read_values('bsl'))
   except Exception as e:
     print(e)
     connection.rollback()
