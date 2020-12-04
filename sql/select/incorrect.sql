589ba6394d8f7dd7e98b66cdfb9fe61a39a90250
tan <ttru702@gmail.com>
2020-12-04 13:29:21 +0900

    update sql select incorrect & unanswered

--- sql/select/incorrect.sql
+++ sql/select/incorrect.sql
@@ -1,5 +1,5 @@
-select w.id, w.word, w.translation, w.explanation, w.example, w.example_translation, w.pronunciation, w.meaning_in_english 
+select w.id, w.word, w.translation, w.explanation, w.example, w.example_translation, w.pronunciation, w.meaning_in_english
 from word w
-left join (select * from test where version=? and version=?) t
-on (w.id=t.word_id)
+left join (select * from test where version_id=?) t
+on w.id=t.word_id
 where "type"=? and t.correct=0;
