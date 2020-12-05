6cd33b6c8e5a130fac6de0018a4e954960116cc6
tan <ttru702@gmail.com>
2020-12-05 11:32:06 +0900

    refactor
    todo:
    version's having category

--- repository.py
+++ repository.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from db_command import select_unanswered
+from db_command import select_incorrect, select_unanswered
 
 # datamodels
 @dataclass
@@ -14,31 +14,43 @@ class Word:
   pronunciation: str
   meaning_in_english: str
 
-# util methods
-def row_to_model(row):
-  return Word(
-    id=row[0],
-    word=row[1],
-    translation=row[2],
-    explanation=row[3],
-    example=row[4],
-    example_translation=row[5],
-    pronunciation=row[6],
-    meaning_in_english=row[7]
-  )
-
-def rows_to_models(rows):
-  models = []
-  for r in rows:
-    models.append(row_to_model(r))
-  return models
-
 # repository
-def get_unanswered_words(version_id, type_):
-  word_rows = select_unanswered(version_id, type_)
-  return rows_to_models(word_rows)
+@dataclass
+class WordRepository:
+  version_id: str
+  category: str
+
+  @staticmethod
+  def row_to_model(row):
+    return Word(
+      id=row[0],
+      word=row[1],
+      translation=row[2],
+      explanation=row[3],
+      example=row[4],
+      example_translation=row[5],
+      pronunciation=row[6],
+      meaning_in_english=row[7]
+    )
+
+  @classmethod
+  def rows_to_models(cls, rows):
+    models = []
+    for r in rows:
+      models.append(cls.row_to_model(r))
+    return models
+
+  def get_unanswered_words(self):
+    word_rows = select_unanswered(self.version_id, self.category)
+    return self.rows_to_models(word_rows)
+
+  def get_incorrect_words(self):
+    print(self.version_id, self.category)
+    word_rows = select_incorrect(self.version_id, self.category)
+    return self.rows_to_models(word_rows)
 
 
 if __name__ == "__main__":
-  words = get_unanswered_words('6027924c-419f-40ae-8b83-454dfa6cd21a', 'ngsl')
-  print(words[0])
+  wr = WordRepository('6027924c-419f-40ae-8b83-454dfa6cd21a', 'ngsl')
+  words = wr.get_unanswered_words()
+  print(words)
