8649bf47ce0e90e3198b58958afb143b5d6f3893
tan <ttru702@gmail.com>
2020-12-08 22:25:11 +0900

    wip _test_roop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import curses
 
-from repository import VersionReository
+from repository import VersionReository, WordRepository
 
 def _versions_menu(screen, versions):
   y_pos = 0
@@ -12,9 +12,9 @@ def _versions_menu(screen, versions):
   screen.keypad(True)
   while True:
     screen.clear()
-    screen.addstr(0, 0, f"idx\tname\tid", curses.A_ITALIC)
+    screen.addstr(0, 0, f"idx\tname\tcomplete\tid", curses.A_ITALIC)
     for i, version in enumerate(versions):
-      screen.addstr(i + 1, 0, f"{i}\t{version.name}\t{version.id}", cp[i == y_pos])
+      screen.addstr(i + 1, 0, f"{i}\t{version.name}\t{version.is_complete()}\t{version.id}", cp[i == y_pos])
     key = screen.getch()
     if key == ord('\n'):
       return versions[y_pos]
@@ -24,14 +24,19 @@ def _versions_menu(screen, versions):
       y_pos = (y_pos - 1) % len(versions)
     screen.refresh()
 
+def _test_loop(screen, word_repository):
+  pass
+
 
 def main(screen):
   curses.start_color()
   vr = VersionReository()
   versions = vr.get_versions()
-  a = _versions_menu(screen, versions)
+  version = _versions_menu(screen, versions)
+  wr = WordRepository(version.id)
+  _test_loop(screen, wr)
   screen.clear()
-  screen.addstr(0, 0, a.name)
+  screen.addstr(0, 0, version.name)
   screen.refresh()
   screen.getkey()
 
